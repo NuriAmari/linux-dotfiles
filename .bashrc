b5a07f33b464d20ef1237dacc81567abfccabdbd
Nuri Amari <nuri.amari99@gmail.com>
2019-12-21 05:35:05 -0500

    Remaps caps to escape in bashrc

--- .bashrc
+++ .bashrc
@@ -1,1 +1,3 @@
-o
+alias vim=$(which nvim)
+alias vi=$(which nvim)
+setxkbmap -option caps:escape
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ln -f config ~/.i3/config
-ln -f .xinitrc ~/.xinitrc
 ln -f .vimrc ~/.vimrc
 ln -f init.vim ~/.config/nvim/init.vim
+ln -f .bashrc ~/.bashrc
